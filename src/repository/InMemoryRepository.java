e0de4cdebc28ceeaaf88bcb559db5d9e3d22aa45
mihaelahrihor <mihaelahrihor@icloud.com>
2024-11-04 16:17:45 +0300

    Created Class InMemoryRepository.

--- src/repository/InMemoryRepository.java
+++ src/repository/InMemoryRepository.java
@@ -2,5 +2,37 @@ package repository;
 
 import model.Identifiable;
 
-public class InMemoryRepository {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
+public class InMemoryRepository<T extends Identifiable> implements IRepository<T> {
+    private final Map<Integer, T> storage = new HashMap<>();
+
+    @Override
+    public void create(T obj) {
+        storage.put(obj.getID(), obj);
+    }
+
+    @Override
+    public T read(int id) {
+        return storage.get(id);
+    }
+
+    @Override
+    public void update(T obj) {
+        storage.put(obj.getID(), obj);
+    }
+
+    @Override
+    public void delete(int id) {
+        storage.remove(id);
+    }
+
+    @Override
+    public List<T> getAll() {
+        return new ArrayList<>(storage.values());
+    }
+
 }
