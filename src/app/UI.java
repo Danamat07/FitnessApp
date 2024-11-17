6b7977ff8d05652bed7687ba48aa5744dc548989
Matei_Dana <dana.matei@stud.ubbcluj.ro>
2024-11-17 15:44:18 +0200

    Created UI and loading data

--- src/app/UI.java
+++ src/app/UI.java
@@ -2,7 +2,7 @@ package app;
 
 import model.*;
 import controller.*;
-import repository.IRepository;
+import repository.InMemoryRepository;
 import service.FitnessService;
 
 import java.time.LocalDate;
@@ -23,11 +23,11 @@ public class UI {
     }
 
     public void trainerUI(int id) {
-        fitnessController.displayAllTrainers();
+        // trainer ui logic
     }
 
     public void memberUI(int id) {
-        fitnessController.displayAllMembers();
+        // member ui logic
     }
 
     public void login() {
@@ -50,12 +50,12 @@ public class UI {
 
             // Check login results and redirect to appropriate UI
             if (member != null) {
-                System.out.println("Welcome, " + member.getName() + " (Member)!");
+                System.out.println("Welcome, " + member.getName() + "!");
                 int memberID = member.getId();
                 memberUI(memberID);
                 isRunning = false; // Exit after successful login
             } else if (trainer != null) {
-                System.out.println("Welcome, " + trainer.getName() + " (Trainer)!");
+                System.out.println("Welcome, " + trainer.getName() + "!");
                 int trainerID = trainer.getId();
                 trainerUI(trainerID);
                 isRunning = false; // Exit after successful login
@@ -102,11 +102,18 @@ public class UI {
 
     public static void main(String[] args) {
         // loading data
-        Trainer t1 = new Trainer("Alex Coroama", "alexcoroama@gmail.com", "0746912336", "weight lifting");
-        Trainer t2 = new Trainer("Liviu Coroama", "liviucoroama@gmail.com", "0733912375", "cardio");
+
+        // trainers
+        Trainer t1 = new Trainer("Sabin Matei", "sabinmatei@gmail.com", "0746912336", "weight lifting");
+        Trainer t2 = new Trainer("Tudor Ratiu", "tudorratiu@gmail.com", "0733912375", "cardio");
         Trainer t3 = new Trainer("Rusai Denis", "rusaidenis@gmail.com", "0734119845", "kickboxing");
         Trainer t4 = new Trainer("Ana Popescu", "anapopescu@gmail.com", "0734829450", "yoga");
         Trainer t5 = new Trainer("Alina Carcea", "alinacarcea@gmail.com", "0745012367", "pilates");
+        t1.setId(1);
+        t2.setId(2);
+        t3.setId(3);
+        t4.setId(4);
+        t5.setId(5);
 
         ArrayList<Trainer> trainersList = new ArrayList<>();
         trainersList.add(t1);
@@ -115,18 +122,27 @@ public class UI {
         trainersList.add(t4);
         trainersList.add(t5);
 
+        // locations
         Location l1 = new Location("Revo Fit", "Str. Manastur nr.24");
         Location l2 = new Location("Revo Star", "Str. Buna Ziua nr.102");
+        l1.setId(1);
+        l2.setId(2);
 
         ArrayList<Location> locationsList = new ArrayList<>();
         locationsList.add(l1);
         locationsList.add(l2);
 
+        // rooms
         Room r1 = new Room("Room 1", 30, l1);
         Room r2 = new Room("Room 2", 25, l2);
         Room r3 = new Room("Room 3", 18, l2);
         Room r4 = new Room("Room 4", 29, l1);
         Room r5 = new Room("Room 5", 31, l1);
+        r1.setId(1);
+        r2.setId(2);
+        r3.setId(3);
+        r4.setId(4);
+        r5.setId(5);
 
         ArrayList<Room> roomList = new ArrayList<>();
         roomList.add(r1);
@@ -135,6 +151,7 @@ public class UI {
         roomList.add(r4);
         roomList.add(r5);
 
+        // members
         ArrayList<FitnessClass> mihaelaClasses = new ArrayList<>();
         Member m1 = new Member("Hrihor Mihaela", "mihaelahrihor@gmail.com", "0758909520", LocalDate.ofYearDay(2024, 12), "Premium", mihaelaClasses);
         m1.setId(1);
@@ -162,44 +179,142 @@ public class UI {
         membersList.add(m4);
         membersList.add(m5);
 
+        // memberships
         ArrayList<Member> studentMs = new ArrayList<>();
         studentMs.add(m2);
         studentMs.add(m5);
 
         Membership studentMembership = new Membership("Student", studentMs, 150);
+        studentMembership.setId(1);
 
         ArrayList<Member> premiumMs = new ArrayList<>();
         premiumMs.add(m1);
         premiumMs.add(m4);
 
         Membership premiumMembership = new Membership("Premium", premiumMs, 250);
+        premiumMembership.setId(2);
 
         ArrayList<Member> basicMs = new ArrayList<>();
         basicMs.add(m3);
 
         Membership basicMembership = new Membership("Basic", basicMs, 200);
+        basicMembership.setId(3);
 
         ArrayList<Membership> membershipList = new ArrayList<>();
         membershipList.add(basicMembership);
         membershipList.add(premiumMembership);
         membershipList.add(studentMembership);
 
-        ArrayList<Feedback> feedbacksfc1 = new ArrayList<>();
+        // equipments
+        ArrayList<FitnessClass> e1FcList = new ArrayList<>();
+        Equipment e1 = new Equipment("weights", 50, e1FcList);
+        e1.setId(1);
 
-        ArrayList<Member> membersfc1 = new ArrayList<>();
+        ArrayList<FitnessClass> e2FcList = new ArrayList<>();
+        Equipment e2 = new Equipment("mattresses", 30, e2FcList);
+        e2.setId(2);
 
-        ArrayList<Equipment> equipmentfc1 = new ArrayList<>();
+        ArrayList<FitnessClass> e3FcList = new ArrayList<>();
+        Equipment e3 = new Equipment("treadmill", 10, e3FcList);
+        e3.setId(3);
 
-        FitnessClass fc1 = new FitnessClass("Yoga", LocalDateTime.of(2024, 11, 15, 10, 30), LocalDateTime.of(2024, 11, 15, 12, 30), t4, r1, 10, r1.getLocation(), feedbacksfc1, membersfc1, equipmentfc1);
+        ArrayList<FitnessClass> e4FcList = new ArrayList<>();
+        Equipment e4 = new Equipment("leg press", 5, e4FcList);
+        e4.setId(4);
 
-        ArrayList<FitnessClass> fcList = new ArrayList<>();
-        fcList.add(fc1);
+        ArrayList<Equipment> equipmentList = new ArrayList<>();
+        equipmentList.add(e1);
+        equipmentList.add(e2);
+        equipmentList.add(e3);
+        equipmentList.add(e4);
 
-        ArrayList<Feedback> feedbackList = new ArrayList<>();
+        // feedbacks
+        ArrayList<Equipment> eqForFClass1 = new ArrayList<>();
+        eqForFClass1.add(e4);
+        ArrayList<Member> f1Members = new ArrayList<>();
+        f1Members.add(m1);
+        f1Members.add(m3);
+        ArrayList<Feedback> fBackForFClass1List = new ArrayList<>();
+        FitnessClass fClass1 = new FitnessClass("kickboxing", LocalDateTime.of(2024, 11, 20, 12, 30), LocalDateTime.of(2024, 11, 20, 14, 30), t3, r2, 18, r2.getLocation(), fBackForFClass1List, f1Members, eqForFClass1);
+        fClass1.setId(1);
+        Feedback fBack1 = new Feedback(m1, fClass1, 4, "Had a great time but the class was a little too exhausting for a beginner");
+        fBack1.setId(1);
+        fBackForFClass1List.add(fBack1);
 
-        ArrayList<Reservation> reservationList = new ArrayList<>();
+        // fitness classes
+        ArrayList<Equipment> eqForFClass2 = new ArrayList<>();
+        eqForFClass1.add(e3);
+        ArrayList<Member> f2Members = new ArrayList<>();
+        f2Members.add(m2);
+        f2Members.add(m4);
+        f2Members.add(m5);
+        ArrayList<Feedback> fBackForFClass2List = new ArrayList<>();
+        FitnessClass fClass2 = new FitnessClass("cardio", LocalDateTime.of(2024, 11, 27, 17, 0), LocalDateTime.of(2024, 11, 27, 18, 0), t2, r1, 11, r1.getLocation(), fBackForFClass2List, f2Members, eqForFClass2);
+        fClass2.setId(2);
+        Feedback fBack2 = new Feedback(m2, fClass2, 2, "The trainer seemed of..");
+        fBack2.setId(2);
+        fBackForFClass2List.add(fBack2);
 
-        // UI ui = new UI(//....);
+        ArrayList<Feedback> feedbackList = new ArrayList();
+        feedbackList.add(fBack1);
+        feedbackList.add(fBack2);
+
+        // reservations
+        Reservation res1 = new Reservation(m1, fClass1, LocalDateTime.of(2024, 11, 20, 12, 30));
+        Reservation res2 = new Reservation(m2, fClass2, LocalDateTime.of(2024, 11, 27, 17, 0));
+        res1.setId(1);
+        res2.setId(2);
+
+        ArrayList<Reservation> reservationsList = new ArrayList<>();
+        reservationsList.add(res1);
+        reservationsList.add(res2);
+
+        // repos, services, controller and ui
+        InMemoryRepository<Equipment> equipmentRepo = new InMemoryRepository<>();
+        equipmentRepo.create(e1);
+        equipmentRepo.create(e2);
+        equipmentRepo.create(e3);
+        equipmentRepo.create(e4);
+        InMemoryRepository<Feedback> feedbackRepo = new InMemoryRepository<>();
+        feedbackRepo.create(fBack1);
+        feedbackRepo.create(fBack2);
+        InMemoryRepository<FitnessClass> fitnessClassRepo = new InMemoryRepository<>();
+        fitnessClassRepo.create(fClass1);
+        fitnessClassRepo.create(fClass2);
+        InMemoryRepository<Location> locationRepo = new InMemoryRepository<>();
+        locationRepo.create(l1);
+        locationRepo.create(l2);
+        InMemoryRepository<Member> memberRepo = new InMemoryRepository<>();
+        memberRepo.create(m1);
+        memberRepo.create(m2);
+        memberRepo.create(m3);
+        memberRepo.create(m4);
+        memberRepo.create(m5);
+        InMemoryRepository<Membership> membershipRepo = new InMemoryRepository<>();
+        membershipRepo.create(basicMembership);
+        membershipRepo.create(studentMembership);
+        membershipRepo.create(premiumMembership);
+        InMemoryRepository<Reservation> reservationRepo = new InMemoryRepository<>();
+        reservationRepo.create(res1);
+        reservationRepo.create(res2);
+        InMemoryRepository<Room> roomRepo = new InMemoryRepository<>();
+        roomRepo.create(r1);
+        roomRepo.create(r2);
+        roomRepo.create(r3);
+        roomRepo.create(r4);
+        roomRepo.create(r5);
+        InMemoryRepository<Trainer> trainerRepo = new InMemoryRepository<>();
+        trainerRepo.create(t1);
+        trainerRepo.create(t2);
+        trainerRepo.create(t3);
+        trainerRepo.create(t4);
+        trainerRepo.create(t5);
+
+        FitnessService service = new FitnessService(equipmentRepo, feedbackRepo, fitnessClassRepo, locationRepo, memberRepo, membershipRepo, reservationRepo, roomRepo, trainerRepo);
+        FitnessController controller = new FitnessController(service);
+
+        UI ui = new UI(controller, membersList, trainersList);
+        ui.login();
 
     }
 
