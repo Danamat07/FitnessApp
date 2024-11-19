26e13729e99d3066fa950dec4e1f5ba8ed1674d7
mihaelahrihor <mihaelahrihor@icloud.com>
2024-11-19 05:40:50 -1000

    Wrote documentation for Identifiable

--- src/model/Identifiable.java
+++ src/model/Identifiable.java
@@ -1,6 +1,20 @@
 package model;
 
+/**
+ * The Identifiable interface provides a contract for objects that can be uniquely
+ * identified by an integer ID. Classes implementing this interface must provide
+ * methods to get and set the ID of the object.
+ */
 public interface Identifiable {
+    /**
+     * Retrieves the unique ID of the object.
+     * @return The unique identifier of the object.
+     */
     int getId();
+
+    /**
+     * Sets the unique ID of the object.
+     * @param id The unique identifier to be assigned to the object.
+     */
     void setId(int id);
 }
